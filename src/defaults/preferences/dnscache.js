f619166a31f53a5d46485edb7a035ce7bb0a0f3a
Dominik Jungowski <Dominik@Jungowski.de>
2008-05-07 22:12:06 +0000

    nach trunk verschoben

---
+++ src/defaults/preferences/dnscache.js
@@ -0,0 +1,6 @@
+// See http://kb.mozillazine.org/Localize_extension_descriptions
+pref("extensions.dnscache@dominik.jungowski.description", "chrome://dnscache/locale/dnscache.properties");
+pref("extensions.dnscache@dominik.jungowski.dnsCacheEntries", 0);
+pref("extensions.dnscache@dominik.jungowski.dnsCacheExpiration", 0);
+pref("network.dnsCacheEntries", 20);
+pref("network.dnsCacheExpiration", 60);
